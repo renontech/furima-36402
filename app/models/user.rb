0dcf0422172eab5c6832d20662c101ebbfdf60dd
renontech <pipipinon2552@gmail.com>
2021-08-09 15:56:52 +0900

    deviseの導入とカラムの追加とビューファイルのコピー

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  with_options presence: true do
+    validates :nickname
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
+end
