5c99b216903c9a66bc68ffcbbf14d279fac57fbb
renontech <pipipinon2552@gmail.com>
2021-08-10 10:49:21 +0900

    ログイン状態かどうかでヘッダーの表示を変える条件分岐を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,15 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   with_options presence: true do
     validates :nickname
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
     validates :birthday
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
+    validates :first_name_kana
+    validates :last_name_kana
   end
 end
