9da8d8056ede47aad9935dbddeabad6a547d8db7
renontech <pipipinon2552@gmail.com>
2021-08-10 15:37:13 +0900

    テストコードの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -37,6 +37,17 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Email can't be blank")
       end
+      it 'emailは@を含まなければ登録出来ない' do
+        @user.email = 'name.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email is invalid")
+      end
+      it 'emailは重複して登録出来ない' do
+        @user1 = FactoryBot.create(:user)
+        @user.email = "#{@user1.email}"
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email has already been taken")
+      end
       it 'passwordが空では登録できない' do
         @user.password = ''
         @user.valid?
