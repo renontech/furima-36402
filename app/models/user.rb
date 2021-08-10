aa25dd0c059824d52b15c2c18af961ae69a3343e
renontech <pipipinon2552@gmail.com>
2021-08-10 15:48:51 +0900

    バリデーションのリファクタリング
    presence: trueをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,17 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
     validates :birthday
-  end
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-    validates :first_name
-    validates :last_name
-  end
-  with_options presence: true, format: { with: /\A[ァ-ヶー]+\z/ } do
-    validates :first_name_kana
-    validates :last_name_kana
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
   end
 end
