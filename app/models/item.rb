d013d681ae148485a57b36bc2ed8d7bf66cdfdc5
renontech <pipipinon2552@gmail.com>
2021-08-11 09:11:48 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,20 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category
+  belongs_to :category, :condition, :prefecture, :shipping, :ship_day
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price, numericality: {in: 300..9999999}
+    validates :image
+    with_options numericality: { other_than: 1, message: "can't be blank" } do
+      validates :category_id
+      validates :condition_id
+      validates :shipping_id
+      validates :prefecture_id
+      validates :ship_day_id
+    end
+  end
 end
