c8f60bfdbd91e835bc21c7d51d7035ba34f42855
renontech <pipipinon2552@gmail.com>
2021-08-11 17:20:13 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :name
     validates :description
-    validates :price, numericality: {in: 300..9999999}
     validates :image
     with_options numericality: { other_than: 1, message: "can't be blank" } do
       validates :category_id
@@ -15,6 +14,9 @@ class Item < ApplicationRecord
       validates :shipping_id
       validates :prefecture_id
       validates :ship_day_id
+    end
+    with_options numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} do
+      validates :price
     end
   end
 end
