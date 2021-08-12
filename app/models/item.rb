a33239875cd0c01398d85129f62578ab101cc74f
renontech <pipipinon2552@gmail.com>
2021-08-12 12:31:25 +0900

    アソシエーションの修正
    ActiveHashのバリューを取り出してビューで表示させるために修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,11 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :category, :condition, :prefecture, :shipping, :ship_day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :shipping
+  belongs_to_active_hash :ship_day
 
   with_options presence: true do
     validates :name
